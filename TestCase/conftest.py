64f5abcca1c70dc9f95e532a55b34e04a2028f9d
17753163583 <17753163583@163.com>
2024-06-25 17:22:11 +0800

    修复调用前置中的登录模块后无法维持app状态的问题

--- TestCase/conftest.py
+++ TestCase/conftest.py
@@ -2,36 +2,39 @@ import json
 import time
 
 import pytest
+import pytest_check as check
 from loguru import logger
 
+from Common.connect_device import connect_device_first
+from Common.find_element import find_element_wrap
 from Common.logger import log_decorator
 
-"""@log_decorator
+
+@log_decorator
 @pytest.fixture(scope='session')
 def login_onelap():
-    driver = connect_device()
+    driver = connect_device_first()
     logger.info("连接设备成功")
     # 首次打开app
-    # find_element(driver, 'id', "com.onelap.bls.dear:id/tv_agree_privacy_dialog").click()
-    find_element(driver, 'id', 'com.onelap.bls.dear:id/btn_login_index').click()
-    find_element(driver, 'id', 'com.onelap.bls.dear:id/et_username_login').send_keys('17753163583')
-    find_element(driver, 'id', 'com.onelap.bls.dear:id/et_password_login').send_keys('zhang107.')
-    find_element(driver, 'id', 'com.onelap.bls.dear:id/btn_check_out_login').click()
+    find_element_wrap(driver, 'id', "com.onelap.bls.dear:id/tv_agree_privacy_dialog").click()
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/btn_login_index').click()
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/et_username_login').send_keys('17753163583')
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/et_password_login').send_keys('zhang107.')
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/btn_check_out_login').click()
 
-    find_element(driver, 'id', 'com.onelap.bls.dear:id/btn_login_login').click()
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/btn_login_login').click()
     # 首次打开app
-    # find_element(driver, 'id', 'com.onelap.bls.dear:id/tv_agree_privacy_dialog').click()
+    find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/tv_agree_privacy_dialog').click()
     logger.info('登录完成')
     time.sleep(2)
 
-    get_app_name = find_element(driver, 'id', 'com.onelap.bls.dear:id/tv_app_name_main_fragment')
+    get_app_name = find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/tv_app_name_main_fragment')
     # 首次打开app
-    # safe_dialog = find_element(driver, 'id', 'com.onelap.bls.dear:id/tv_confirm_common_dialog').click()
+    # find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/tv_confirm_common_dialog').click()
     if check.equal(get_app_name.text, '顽鹿运动（预发布服）'):
         logger.info("home_app_name断言成功")
     else:
         logger.error("home_app_name断言失败")
-"""
 
 
 @log_decorator
@@ -52,4 +55,3 @@ def get_boss_cookies(driver):
         logger.info("保存cookies到本地")
     logger.info("删除cookies成功")
     driver.delete_all_cookies()
-
--- TestCase/test_onelap_report.py
+++ TestCase/test_onelap_report.py
@@ -3,7 +3,7 @@ import time
 import pytest_check as check
 from selenium.webdriver import ActionChains
 
-from Common.connect_device import connect_device
+from Common.connect_device import connect_device_later
 from Common.find_element import find_element_wrap
 from Common.logger import log_decorator
 
@@ -12,16 +12,16 @@ class TestOnelap:
 
     @log_decorator
     def test_report(self, login_onelap):
-        driver = connect_device()
+        driver = connect_device_later()
         actions = ActionChains(driver)
 
         # 初次登录时使用
-        # find_element(driver, 'id', "com.android.packageinstaller:id/permission_allow_button").click()
+        # find_element_wrap(driver, 'id', "com.android.packageinstaller:id/permission_allow_button").click()
 
         find_element_wrap(driver, 'id', "com.onelap.bls.dear:id/include_planet_bottom_navigation").click()
 
         # 关闭第一次打开路线页的安全提示(登录完成后会切换页面，容易造成异常)
-        # find_element(driver, 'id', 'com.onelap.bls.dear:id/tv_confirm_common_dialog').click()
+        find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/tv_confirm_common_dialog').click()
 
         # 打开搜索框
         find_element_wrap(driver, 'id', 'com.onelap.bls.dear:id/tv_search').click()
@@ -30,13 +30,13 @@ class TestOnelap:
         driver.keyevent(66)
 
         find_element_wrap(driver, 'xpath',
-                     '//androidx.recyclerview.widget.RecyclerView[@resource-id="com.onelap.bls.dear:id/rv_route_list"]/android.view.ViewGroup').click()
+                          '//androidx.recyclerview.widget.RecyclerView[@resource-id="com.onelap.bls.dear:id/rv_route_list"]/android.view.ViewGroup').click()
 
-        time.sleep(10)
+        time.sleep(5)
 
         find_element_wrap(driver, 'xpath', '//android.view.View[@text="骑友评论"]').click()
         content = find_element_wrap(driver, 'xpath',
-                               '//android.view.View[@resource-id="content-wrap"]/android.view.View[3]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[3]')
+                                    '//android.view.View[@resource-id="content-wrap"]/android.view.View[3]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[3]')
 
         actions.click_and_hold(content).pause(2).release().perform()
 
@@ -52,4 +52,3 @@ class TestOnelap:
         # 使用是否存在来捕获toast，是否可见无法捕获toast
         # WebDriverWait(driver, 10, 0.25).until(ec.presence_of_element_located(('xpath', '//android.widget.Toast[@text="你的举报我们已成功收到，会尽快核实处理"]')))
         # print("测试成功。。。。。")
-
