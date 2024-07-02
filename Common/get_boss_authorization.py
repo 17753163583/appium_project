5d330dbf0f420b533c8708f47ffbff82006ab363
17753163583 <17753163583@163.com>
2024-07-02 14:56:16 +0800

    添加用例

--- Common/get_boss_authorization.py
+++ Common/get_boss_authorization.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_boss_authorization():
+def get_onelap_authorization():
     url = 'https://rfs-fitness-informal.rfsvr.net/api/account/v1/login'
     body = {"account": "17753163583", "password": "dc158e485dba3cb7a6cfc9063568ac9e"}
 
@@ -11,4 +11,4 @@ def get_boss_authorization():
 
 
 if __name__ == '__main__':
-    get_boss_authorization()
+    get_onelap_authorization()
